b350fdf994191fd9985c3da6867c5be0fd296bad
heliy <ohhlyhly@gmail.com>
2021-02-02 23:50:11 +0800

    1-16

--- ch1/e16printlongest.c
+++ ch1/e16printlongest.c
@@ -13,33 +13,12 @@ main()
   int totallen=0;
   int max;
   char line[MAXLINE];
-  char* longest;
-  char* front;
 
   max=0;
   while((thislen=mygetline(line,MAXLINE))>0)
    { 
-    totallen+=thislen;
-    char temp[totallen];
-    kaicopy(temp,front,0);
-    kaicopy(temp,line,totallen-thislen);
-
-    if(thislen==MAXLINE && line[MAXLINE-1]!='\n')/*this line isn't complete */
-	front=&temp;
-    else
-      {
-        if(totallen>max)
-         {
-            max=totallen;
-	    longest=&temp;
-         }
-        totallen=0;
-	char end="\0";
-	front=&end;
-      }
-}
-  if(max>0)
-    printf("%s\n%d\n",longest,max);
+    printf("%d\n",thislen-1);
+   }
   return 0;
 }
 
